30c615789fb94c2d297e61169394bb65b25b6c4b
da5nsy <dannygarside@outlook.com>
2019-08-01 18:03:46 +0100

    broken something, what is it?

--- Data Analysis/VonKriesTest.m
+++ Data Analysis/VonKriesTest.m
@@ -6,14 +6,17 @@
 
 clear, clc, close all
 
-plt = 0;
+plt = 1;
 
 % Load observer
 load T_cones_ss10.mat
 load T_rods.mat
-T_obs = [T_cones_ss10; SplineCmf(S_rods,T_rods,S_cones_ss10)];
+load T_melanopsin.mat
+T_obs = [T_cones_ss10;...
+    SplineCmf(S_rods,T_rods,S_cones_ss10);...
+    SplineCmf(S_melanopsin,T_melanopsin,S_cones_ss10)];
 S_obs = S_cones_ss10;
-clear T_cones_ss10 S_cones_ss10 T_rods S_rods %cleanup
+clear T_cones_ss10 S_cones_ss10 T_rods S_rods T_melanopsin S_melanopsin %cleanup
 %figure, plot(SToWls(S_obs),T_obs')
 
 %% Grabbing bits to make an empty data container
@@ -36,11 +39,11 @@ Lval = 85:-5:10;                    % lightness values
 %TNM = floor(min(TI(4,:)/60));      % find length of shortest session (minutes)
 TNM = 72;                           % (DG note: This is what the real 2013 data ends up as)
 
-LMSR_sim = zeros(4,LN,TNM,WN,'double'); % Empty data container (Should be 4,16,72,16)
+LMSR_sim = zeros(size(T_obs,1),LN,TNM,WN,'double'); % Empty data container (Should be 4,16,72,16)
 
 % SO, what do these number mean?
 
-% 4 - Long, Medium, Short, Rod
+% 4 - Long, Medium, Short, Rod, Mel
 % 16 - number of lightness levels (LN)
 % 72 - minutes of session
 % 16 - number of wavelength samples
@@ -68,12 +71,12 @@ end
 
 lness = 6:11;              % lightness (can be single or multiple)
 LMSI_sim=squeeze(mean(LMSR_sim(:,lness,:,:),2));
-labels = {'L','M','S'};
+labels = {'L','M','S','R','I'};
 
 if plt
     figure('units','normalized','outerposition',[0 0 1 1])
-    for i=1:3
-        subplot(1,3,i)
+    for i=1:size(T_obs,1)
+        subplot(1,size(T_obs,1),i)
         imagesc(squeeze(LMSI_sim(i,:,:)))
         set(gca,'YDir','normal')
         %colorbar
@@ -89,8 +92,7 @@ if plt
 end
 
 %% Load a real dataset
-
-load 'Tania LMSI for testing VonKriesTest'
+load 'C:\Users\cege-user\Dropbox\UCL\Data\LargeSphere\Data Analysis\Tania LMSI for testing VonKriesTest'
 LMSI_real = LMSI; clear LMSI
 
 if plt
@@ -117,13 +119,16 @@ rng(1)
 
 for q = 1:100 % Run it X number of times to see whether difference between different values of nIn is just noise or something meaningful
     
-    nIn = 4;            %number of inputs (L,M,S,R,I in that order)
+    nIn = 5;            %number of inputs (L,M,S,R,I in that order)
     nOut = 3;
     runs = 1000;
     SFM = 50;           %Scaling factor max
     
-    for i=1:nIn
-        CWstore{i} = zeros(nIn,1);  %Cone weight store
+    for i=1:nIn % This creates duplicate data and no longer needs to be a cell (previously I thresholded the data that I stored based on correlation which left different columns with different lengths, but now we keep everything.
+        CWstore{i} = zeros(nIn,runs);  %Cone weight store
+    end
+    
+    for i=1:nOut
         Cstore{i} = 0;              %Correlation store
     end
     
@@ -145,9 +150,11 @@ for q = 1:100 % Run it X number of times to see whether difference between diffe
         
         % Compare random effect with our actual data
         LMSI_real_slim = squeeze(mean(LMSI_real,2));
-        for k=1:nOut % Just trying to predict L,M,S (for now)
-            corrcoef(randomEffect_slim,LMSI_real_slim(k,:)');
+        for k=1:nIn
             CWstore{k} = cat(2,CWstore{k},ConeWeights);
+        end
+        for k=1:nOut % Just trying to predict L,M,S (for now)
+            
             Cstore{k} = cat(2,Cstore{k},...
                 min(min(corrcoef(randomEffect_slim,LMSI_real_slim(k,:)'))));
         end
@@ -165,16 +172,12 @@ for q = 1:100 % Run it X number of times to see whether difference between diffe
     end
 end
 
-mean(mc,2)'
-max(mc')
+disp(mean(mc,2)')
+disp(max(mc'))
 
 %% Visualise best performance
 
-close all
-
 figure,
-
-
 for k=1:nOut
     subplot(2,nOut,k)
     [~,j] = max(abs(Cstore{1,k}));
@@ -206,4 +209,12 @@ for i=1:nOut
     colorbar
 end
 
-%%% figure, scatter(randomEffect(1,:),LMSI_real_slim(1,:))
+%%
+%figure, scatter(randomEffect(1,:),LMSI_real_slim(1,:))
+
+%%
+
+figure,
+subplot(1,nOut,1)
+plot
+
